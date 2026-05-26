b955fd2bfa8a6a429ff5b4ee20507a56a177075e
propanfloete7687 <ingoburgold87@gmail.com>
2026-05-26 07:26:35 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'step-aerobic-v2';
+const CACHE = 'step-aerobic-v3';
 const FILES = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
