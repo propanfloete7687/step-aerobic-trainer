55e46963f4d7db20ecbbc1cf9cd526d78611bf84
propanfloete7687 <ingoburgold87@gmail.com>
2026-05-26 06:51:48 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,42 +1,16 @@
 const CACHE = 'step-aerobic-v2';
+const FILES = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
-const FILES = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icon-192.png',
-  '/icon-512.png',
-  '/screenshot_1_planner.png',
-  '/screenshot_2_workout.png',
-  '/screenshot_3_history.png',
-  '/screenshot_4_coach.png'
-];
-
-// Installation – alle Dateien in den Cache laden
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(FILES))
-  );
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
   self.skipWaiting();
 });
-
-// Aktivierung – alten Cache löschen
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.filter(key => key !== CACHE).map(key => caches.delete(key))
-      )
-    )
-  );
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
   self.clients.claim();
 });
-
-// Fetch – erst Cache, dann Netzwerk
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(cached => {
-      return cached || fetch(event.request).catch(() => caches.match('/index.html'));
-    })
-  );
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('/index.html'))));
 });
